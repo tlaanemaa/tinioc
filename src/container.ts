5c58ed0fd0700f62dbe8c56b9a039fb9532a154e
Taavi Laanemaa <taavi.laanemaa@klarna.com>
2021-09-16 22:09:52 +0200

    Added the main DI container

---
+++ src/container.ts
@@ -0,0 +1,36 @@
+import { ID, FactoryOf } from "./types";
+
+export class Container {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private readonly dependencies: Record<ID, FactoryOf<any>> = {};
+
+  /**
+   * Register a new dependency
+   */
+  public register<T>(id: ID, dependency: FactoryOf<T>): this {
+    this.dependencies[id] = dependency;
+    return this;
+  }
+
+  /**
+   * Returns a cloned instance of the container
+   */
+  public clone(): Container {
+    const clonedContainer = new Container();
+    Object.entries(this.dependencies).forEach(([id, dependency]) =>
+      clonedContainer.register(id, dependency)
+    );
+    return clonedContainer;
+  }
+
+  /**
+   * Get a dependency
+   */
+  public get<T>(id: ID): T {
+    const dependency = this.dependencies[id];
+    if (dependency === undefined)
+      throw new Error(`Dependency "${String(id)}" not found!`);
+
+    return dependency({ get: this.get.bind(this) });
+  }
+}
