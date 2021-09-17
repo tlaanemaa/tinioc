b0e7a6f413750f569c5b47ed557536f844e8933a
Taavi Laanemaa <taavi.laanemaa@klarna.com>
2021-09-17 21:46:17 +0200

    Added tests for different dependency relationships

--- src/container.spec.ts
+++ src/container.spec.ts
@@ -1,36 +1,99 @@
 import { Container } from "./container";
-import { declareDependency } from "./utils";
+import { declareDependency, DependencyNotFoundError } from "./utils";
 
 describe("Container", () => {
-  const ADDER = Symbol.for("adder");
-  interface Adder {
-    add(num1: number): number;
-  }
+  describe("when used with simple dependency relationships", () => {
+    // Dependency declarations
+    const ADDER = Symbol.for("adder");
+    interface Adder {
+      add(number: number): number;
+    }
 
-  const NUMBER_CARRIER = Symbol.for("number_carrier");
-  interface NumberCarrier {
-    num2: number;
-  }
+    const NUMBER_CARRIER = Symbol.for("number_carrier");
+    interface NumberCarrier {
+      num2: number;
+    }
 
-  const adder = declareDependency(({ get }): Adder => {
-    const numberCarrier = get<NumberCarrier>(NUMBER_CARRIER);
-    const add = (number: number) => number + numberCarrier.num2;
-    return { add };
+    // Dependency implementations
+    const adder = declareDependency(({ get }): Adder => {
+      const numberCarrier = get<NumberCarrier>(NUMBER_CARRIER);
+      const add = (number: number) => number + numberCarrier.num2;
+      return { add };
+    });
+
+    const numberCarrier = declareDependency((): NumberCarrier => {
+      return { num2: 5 };
+    });
+
+    // Dependency bindings
+    const container = new Container();
+    container.bind<Adder>(ADDER, adder);
+    container.bind<NumberCarrier>(NUMBER_CARRIER, numberCarrier);
+
+    describe("when asked for a known dependency", () => {
+      const dependency = container.get<Adder>(ADDER);
+
+      it("should return that dependency and resolve it's dependencies", () => {
+        expect(dependency.add(7)).toBe(5 + 7);
+      });
+    });
+
+    describe("when asked for an unknown dependency", () => {
+      it("should throw an error", () => {
+        expect(() => container.get<Adder>("banana")).toThrow(
+          DependencyNotFoundError
+        );
+      });
+    });
   });
 
-  const numberCarrier = declareDependency((): NumberCarrier => {
-    return { num2: 5 };
-  });
+  describe("when used with circular dependency relationships", () => {
+    // Dependency declarations
+    const ADDER = Symbol.for("adder");
+    interface Adder {
+      baseValue: number;
+      add(number: number): number;
+    }
 
-  const container = new Container();
-  container.bind<Adder>(ADDER, adder);
-  container.bind<NumberCarrier>(NUMBER_CARRIER, numberCarrier);
+    const NUMBER_CARRIER = Symbol.for("number_carrier");
+    interface NumberCarrier {
+      getNum2(): number;
+    }
 
-  describe("when asked for a dependency", () => {
-    const dependency = container.get<Adder>(ADDER);
+    // Dependency implementations
+    const adder = declareDependency(({ get }): Adder => {
+      const numberCarrier = get<NumberCarrier>(NUMBER_CARRIER);
+      const add = (number: number) => number + numberCarrier.getNum2();
+      return { add, baseValue: 3 };
+    });
 
-    it("should return that dependency and resolve it's dependencies", () => {
-      expect(dependency.add(7)).toBe(12);
+    const numberCarrier = declareDependency(({ get }): NumberCarrier => {
+      const getNum2 = () => {
+        const adder = get<Adder>(ADDER);
+        return adder.baseValue + 1;
+      };
+      return { getNum2 };
+    });
+
+    // Dependency bindings
+    const container = new Container();
+    container.bind<Adder>(ADDER, adder);
+    container.bind<NumberCarrier>(NUMBER_CARRIER, numberCarrier);
+
+    describe("when asked for a known dependency", () => {
+      const dependency = container.get<Adder>(ADDER);
+
+      it("should return that dependency and resolve it's dependencies", () => {
+        expect(dependency.add(7)).toBe(3 + 1 + 7);
+      });
+    });
+
+    describe("when asked for an unknown dependency", () => {
+      it("should throw an error", () => {
+        expect(() => container.get<Adder>(Symbol.for("potatis"))).toThrow(
+          DependencyNotFoundError
+        );
+      });
     });
   });
 });
