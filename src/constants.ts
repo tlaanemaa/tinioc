85d632f717f75e5175e955d4ed8b088933162c90
Taavi Laanemaa <taavi.laanemaa@klarna.com>
2021-10-12 22:22:48 +0200

    Added scopes constants

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * A collection of component registration scopes
+ */
+export const SCOPES = {
+  transient: "transient",
+  request: "request",
+  singleton: "singleton",
+} as const;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export { Container } from "./container";
 export { ID, Inject, FactoryOf } from "./types";
+export { SCOPES } from "./constants";
 export { BindingNotFoundError } from "./binding-not-found-error";
